5aff09fa3a7d748c2b10b529b14e6d3f9ef79398
DavidRnR <carlosdavidmartin@gmail.com>
2020-01-28 11:44:23 -0300

    Html home nicer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {
   MatInputModule, MatCardModule, MatFormFieldModule,
-  MatProgressSpinnerModule, MatButtonModule, MatListModule, MatToolbarModule
+  MatProgressSpinnerModule, MatButtonModule, MatListModule, MatToolbarModule, MatProgressBarModule
 } from '@angular/material';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -41,6 +41,7 @@ import { DataService } from './services/data.service';
     MatCardModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
+    MatProgressBarModule,
     MatListModule,
     MatToolbarModule
   ],
